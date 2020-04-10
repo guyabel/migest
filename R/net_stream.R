445185d7cc22e0482fe71a98cdc27eeb53ddddf5
Guy Abel <g.j.abel@gmail.com>
2020-04-10 15:45:53 +0800

    un manual bits

---
+++ R/net_stream.R
@@ -0,0 +1,30 @@
+#' Calculate Counterstream and Stream Net Migration
+#'
+#' @param m A \code{matrix} or data frame of origin-destination flows. For \code{matrix} the first and second dimensions correspond to origin and destination respectively. For a data frame the first and second columns correspond to origin and destination respecivly. The third column contains the number of migraitons or migrants.
+#'
+#' @return A \code{tibble} with columns for orig, destination, corridor, stream, countersteam and net stream in each pair (corridor)
+#' @export
+#'
+#' @examples
+net_stream <- function(m){
+  if(!is.matrix(m)){
+    d <- m %>%
+      dplyr::rename(orig = 1, 
+                    dest = 2, 
+                    stream = 3)
+  }
+  if(is.matrix(m)){
+    d <- as.data.frame.table(x = m, responseName = "stream", stringsAsFactors = FALSE) %>%
+      dplyr::rename(orig = 1, 
+                    dest = 2) 
+  }
+  d %>%
+    dplyr::as_tibble() %>%
+    filter(orig != dest) %>%
+    mutate(corridor = ifelse(orig < dest, paste(orig, dest, sep = " - "), paste(dest, orig, sep = " - "))) %>%
+    group_by(corridor) %>%
+    mutate(counterstream = rev(stream),
+           stream_net = stream - counterstream) %>%
+    ungroup() %>%
+    select(orig, dest, corridor, everything())
+}
