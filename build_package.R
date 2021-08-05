949e5afb9273e62febe70a6edb76edc53369af23
Guy Abel <g.j.abel@gmail.com>
2021-08-05 16:25:14 +0200

    gh actions

--- build_package.R
+++ build_package.R
@@ -15,6 +15,9 @@ roxygen2::roxygenise()
 devtools::check()
 devtools::build()
 
+usethis::use_github_action_check_standard()
+usethis::use_github_action("pkgdown")
+usethis::use_github_actions_badge()
 file.show("NEWS.md")
 
 # usethis::use_pkgdown()
